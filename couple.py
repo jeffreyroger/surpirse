436372cc004f87b883cc0158da3849e9cb2284b8
jeffreyrogerworks <jeffreyroger206@gmail.com>
2025-12-11 21:13:58 +0530

    Bind Flask to 0.0.0.0 for deployment

--- couple.py
+++ couple.py
@@ -117,4 +117,5 @@ def secret():
 # ----------------------------------------------------------
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=5000,debug=True)
